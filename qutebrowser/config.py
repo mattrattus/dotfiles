8b4622a779980a6d416c6353aa7378c6ca1aff14
Matt <79484060+mattrattus@users.noreply.github.com>
2021-02-23 12:24:37 +0100

    first

---
+++ qutebrowser/config.py
@@ -0,0 +1,39 @@
+config.load_autoconfig()
+
+c.editor.command = ['nvim', '{}']
+
+c.content.canvas_reading = False
+
+c.content.cookies.accept = 'no-3rdparty'
+
+c.content.cookies.store = False
+
+c.content.geolocation = False
+
+c.content.headers.do_not_track = True
+
+c.content.headers.referer = 'same-domain'
+
+c.content.headers.user_agent = 'Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0'
+
+c.content.javascript.enabled = False
+
+c.content.private_browsing = True
+
+c.content.webgl = False
+
+c.content.webrtc_ip_handling_policy = 'default-public-interface-only'
+
+c.url.default_page = 'about:blank'
+
+c.url.start_pages = 'about:blank'
+
+c.downloads.position = 'bottom'
+
+c.search.incremental = False
+
+c.tabs.position = "left"
+
+c.tabs.select_on_remove = "next"
+
+c.tabs.width = "10%"
